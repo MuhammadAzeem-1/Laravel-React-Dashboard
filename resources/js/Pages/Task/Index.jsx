2af1feb3c64c743a7b871111ecabb1d126aa2e8f
Muhammad Azeem <mazeem182000@gmail.com>
2024-07-26 15:18:47 +0500

    first commit

---
+++ resources/js/Pages/Task/Index.jsx
@@ -0,0 +1,53 @@
+import Pagination from "@/Components/Pagination";
+import Authenticated from "@/Layouts/AuthenticatedLayout";
+import { Head, Link, router } from "@inertiajs/react";
+import { TASK_STATUS_TEXT_MAP , TASK_STATUS_CLASS_MAP} from "@/Constansts.jsx";
+import TextInput from "@/Components/TextInput";
+import SelectInput from "@/Components/SelectInput";
+import TableHeading from "@/Components/TableHeading";
+import TasksTable from "./TasksTable";
+
+export default function Index({ auth, tasks, queryParams = null,  }) {
+    console.log(tasks);
+    queryParams = queryParams || {};
+
+
+    
+    
+
+  return (
+    <Authenticated
+      user={auth.user}
+      header={
+        <div className="flex justify-between items-center">
+          <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            Tasks
+          </h2>
+          <Link
+            href={route("task.create")}
+            className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600"
+          >
+            Add new
+          </Link>
+        </div>
+      }
+    >
+      <Head title="Tasks" />
+
+      <div className="py-12">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          {/* {success && (
+            <div className="bg-emerald-500 py-2 px-4 text-white rounded mb-4">
+              {success}
+            </div>
+          )} */}
+          <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div className="p-6 text-gray-900 dark:text-gray-100">
+               <TasksTable tasks={tasks} queryParams={queryParams}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Authenticated>
+  );
+}
